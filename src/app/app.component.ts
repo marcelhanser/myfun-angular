4723d083680f25a4184cc9a43f3df0f96a52df37
Marcel Hanser <marcel.hanser@zuehlke.com>
2017-10-06 20:20:35 +0200

    Add git ignore and other stuff

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+
+  constructor(private http: HttpClient) {
+  }
+
   title = 'app';
+  results: any;
+
+  fetchData(): void {
+    this.http.get('https://0myff4njvi.execute-api.eu-west-1.amazonaws.com/Stage').subscribe(data => {
+      // Read the result field from the JSON response.
+      this.results = data;
+    });
+  }
 }
